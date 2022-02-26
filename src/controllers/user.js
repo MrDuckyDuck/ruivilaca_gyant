1690abb087719853cb2b05780feb86bf23062a17
ruivilaca <rui.vilaca@peekmed.com>
2022-02-26 13:11:43 +0000

    Added login route with input validations and token generation

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,8 +1,37 @@
+const User = require("../models/user");
+
 /* @desc    Route to login user.
             Returns a token
 */
 exports.postLogin = async ctx => {
-    ctx.response.body = "Login"
+    const {email, password} = ctx.request.body;
+
+    /* Validate if user provided an email and a password */
+    if(!email && !password) {
+        return ctx.throw(401, "Invalid credentials.")
+    }
+
+    /* Search for an user and, if it exists, returns its password to compare it with the one provided */
+    const user = await User.findOne({email}).select("+password +session");
+    if(!user) {
+        return ctx.throw(401, "Invalid credentials.")
+    }
+
+    /* Checks if user is already logged in. */
+    if(user.session) {
+        return ctx.throw(401, "User already logged in.");
+    }
+
+    /* Compares password. If ok, returns true  */
+    const token = await user.comparePassword(password) ? await user.generateToken() : null;
+    if (!token) {
+        return ctx.throw(401, "Invalid credentials." )
+    }
+
+    /* Marks that user has an active session to avoid multiple sessions with the same account */
+    await User.updateOne({_id: user._id}, {session: true}, {runValidators: true})
+
+    ctx.response.body = token
 }
 
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 /**
  * @desc    Data schema for Condition Model
@@ -28,6 +29,17 @@ const userSchema = new mongoose.Schema({
     }
 }, {versionKey: false} );
 
+
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+/* Generate an Access Token - In this case the token's TTL will be huge (1 day) to facilitate the test case. */
+userSchema.methods.generateToken = function () {
+    const user = this;
+    const token = jwt.sign({ sub: user._id }, "secret123", { expiresIn: 24 * 60 * 60 });
+    return token;
+}
 
 /* Hash the plain text password before saving */
 userSchema.pre('save', async function (next) {
