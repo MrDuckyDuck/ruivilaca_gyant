acf8fb7a1e383cebe9cd76259aa829437e453665
ruivilaca <rui.vilaca@peekmed.com>
2022-02-26 16:32:49 +0000

    Added GET /unreviewed case

--- src/controllers/ehr.js
+++ src/controllers/ehr.js
@@ -7,6 +7,17 @@ exports.getAllEhrCases = async ctx => {
     ctx.response.body = { cases: ehrCases };
 }
 
+
+/* @desc    Route a single case unreviewed */
+/* @note    This could be a single GET ALL route accepting different parameters and performing a flexible get with different parameters combination
+            Made this way to simplify the code
+*/
+exports.getUnreviewedCase = async ctx => {
+    /* Fetch using lean to increase performance & because it is not necessary a mongoose object */
+    const ehrCase = await Ehr.find({label: null}).limit(1).lean();
+    ctx.response.body = { case: ehrCase.length > 0 ? ehrCase[0] : []  };
+}
+
 /* @desc    Route to logout user.
 */
 exports.updateEhrCaseById = async ctx => {
--- src/routes/ehr.js
+++ src/routes/ehr.js
@@ -1,5 +1,5 @@
 const KoaRouter = require("koa-router");
-const {getAllEhrCases, updateEhrCaseById} = require("../controllers/ehr");
+const {getAllEhrCases, getUnreviewedCase, updateEhrCaseById} = require("../controllers/ehr");
 
 /* Adds a prefix to all routes on this file so there is no need to repeat it on every route */
 const router = new KoaRouter({
@@ -7,7 +7,8 @@ const router = new KoaRouter({
 });
 
 
-router.get("/", getAllEhrCases);
-router.put("/:id", updateEhrCaseById);
+router.get("/", getAllEhrCases );
+router.get("/unreviewed", getUnreviewedCase );
+router.put("/:id", updateEhrCaseById );
 
 module.exports.EhrRouter = router
